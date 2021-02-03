ba764a094023436762311fda199b37492db0ba97
Minh <minhvn29@gmail.com>
2021-02-03 13:23:56 -0800

    Complete custom hooks for animal and breeds dropdown

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react'; 
+import { ANIMALS } from '@frontendmasters/pet';
+import useDropdown from './useDropdown';
+
 
 const SearchParams = () => {
   const [location, setLocation] = useState('San Jose, CA');
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds)
 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} onChange={e => setLocation(e.target.value)} placeholder="location"/>
+          <input id="location" value={location} onChange={e => setLocation(e.target.value)} placeholder="location" />
         </label>
+        <AnimalDropdown/>
+        <BreedDropdown/>
         <button>Submit</button>
       </form>
     </div>
