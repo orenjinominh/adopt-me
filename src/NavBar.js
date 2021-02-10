c1c309e0d350b700f61ffcbb68ac56644765ef30
Minh <minhvn29@gmail.com>
2021-02-09 17:53:59 -0800

    Complete Emotion CSS styling

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from "@reach/router";
-import { css } from "@emotion/core";
+import { css, keyframes} from "@emotion/core";
+import colors from './colors';
 
-const NavBar = () => (
-  <header
-    css={css`
-      background-color: #333;
-      position: sticky;
-      top: 0;
-      z-index: 10;
-    `}
-  >
-    <Link to="/">Adopt Me!</Link>
-    <span aria-label="logo" role="img">🐩s</span>
-  </header>
-);
+const spin = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const NavBar = () => {
+
+  const [padding] = useState(15);
+
+  return (
+    <header
+      css={css`
+        background-color: ${colors.secondary};
+        padding: ${padding}px;
+        position: sticky;
+        top: 0;
+        z-index: 10;
+      `}
+    >
+      <Link to="/">Adopt Me!</Link>
+      <span 
+        css={css`
+          font-size: 60px;
+          display: inline-block;
+          animation: 1s ${spin} linear infinite reverse;
+
+          &:hover {
+            text-decoration: underline; 
+          }
+        `}
+        aria-label="logo"
+        role="img"
+      >🐩</span>
+    </header>
+  )
+
+}
+
+
+
+
+
 
 export default NavBar;
